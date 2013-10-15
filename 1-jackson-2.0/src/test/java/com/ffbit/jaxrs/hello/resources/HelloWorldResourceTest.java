5d3c7099cf782c7bc490a7377f0e95e84679406d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-10-15 22:42:02 +0300

    Hello World JAX-RS 2.0 Jersey Application with Jackson 2.0 Switched to Lightweight HTTP Server

--- 1-jackson-2.0/src/test/java/com/ffbit/jaxrs/hello/resources/HelloWorldResourceTest.java
+++ 1-jackson-2.0/src/test/java/com/ffbit/jaxrs/hello/resources/HelloWorldResourceTest.java
@@ -1,16 +1,10 @@
 package com.ffbit.jaxrs.hello.resources;
 
 import com.ffbit.jaxrs.hello.dto.Greeting;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.client.filter.LoggingFilter;
-import com.sun.jersey.api.json.JSONConfiguration;
-import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.spi.client.ClientFactory;
-import com.sun.jersey.test.framework.spi.container.TestContainer;
 import com.sun.jersey.test.framework.spi.container.TestContainerException;
+import com.sun.jersey.test.framework.spi.container.TestContainerFactory;
+import com.sun.jersey.test.framework.spi.container.http.HTTPContainerFactory;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
@@ -23,6 +17,11 @@ public class HelloWorldResourceTest extends JerseyTest {
     public HelloWorldResourceTest() throws TestContainerException {
         super("com.ffbit.jaxrs.hello.resources", "com.ffbit.jaxrs.hello.providers");
 
+    }
+
+    @Override
+    protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+        return new HTTPContainerFactory();
     }
 
     @Test
