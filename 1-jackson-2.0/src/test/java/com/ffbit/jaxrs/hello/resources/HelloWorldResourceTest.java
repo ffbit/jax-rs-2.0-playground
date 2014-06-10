b783f4cdb9412629a6d42bf0335a8c0219e02085
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-06-10 11:29:34 +0300

    Fixed projects' build.

--- 1-jackson-2.0/src/test/java/com/ffbit/jaxrs/hello/resources/HelloWorldResourceTest.java
+++ 1-jackson-2.0/src/test/java/com/ffbit/jaxrs/hello/resources/HelloWorldResourceTest.java
@@ -1,12 +1,14 @@
 package com.ffbit.jaxrs.hello.resources;
 
 import com.ffbit.jaxrs.hello.dto.Greeting;
-import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.spi.container.TestContainerException;
-import com.sun.jersey.test.framework.spi.container.TestContainerFactory;
-import com.sun.jersey.test.framework.spi.container.http.HTTPContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
 import org.junit.Test;
 
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -14,20 +16,22 @@ import static org.junit.Assert.assertThat;
 
 public class HelloWorldResourceTest extends JerseyTest {
 
-    public HelloWorldResourceTest() throws TestContainerException {
-        super("com.ffbit.jaxrs.hello.resources", "com.ffbit.jaxrs.hello.providers");
-
+    @Override
+    protected Application configure() {
+        return new ResourceConfig()
+                .packages("com.ffbit.jaxrs.hello.resources", "com.ffbit.jaxrs.hello.providers");
     }
 
     @Override
     protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
-        return new HTTPContainerFactory();
+        return new GrizzlyTestContainerFactory();
     }
 
     @Test
     public void itShouldReturnTextPlainResponse() throws Exception {
-        String response = resource()
+        String response = target()
                 .path("hello")
+                .request()
                 .accept(MediaType.TEXT_PLAIN)
                 .get(String.class);
 
@@ -36,8 +40,9 @@ public class HelloWorldResourceTest extends JerseyTest {
 
     @Test
     public void itShouldReturnApplicationJsonResponse() throws Exception {
-        Greeting response = resource()
+        Greeting response = target()
                 .path("hello")
+                .request()
                 .accept(MediaType.APPLICATION_JSON)
                 .get(Greeting.class);
 
